d0d8778e5b5cb59a1ca61011cf3c9cf899fbc97a
Line Moseng <line.moseng@nav.no>
2022-09-22 11:11:33 +0200

    Initial setup

---
+++ src/main/kotlin/no/nav/Application.kt
@@ -0,0 +1,12 @@
+package no.nav
+
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import no.nav.plugins.configureRouting
+
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}

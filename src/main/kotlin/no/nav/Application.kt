092910478b71bcef06df42e8ba17d44a9ffd7fd2
Line Moseng <line.moseng@nav.no>
2022-09-23 10:14:44 +0200

    klint

--- src/main/kotlin/no/nav/Application.kt
+++ src/main/kotlin/no/nav/Application.kt
@@ -4,7 +4,6 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import no.nav.plugins.configureRouting
 
-
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()

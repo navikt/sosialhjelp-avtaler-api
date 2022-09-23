6d9d3f96639e2c39f42709bd85f331ab706727d6
Line Moseng <line.moseng@nav.no>
2022-09-23 12:13:04 +0200

    fix indentation

---
+++ src/main/kotlin/no/nav/internal/SelftestRoutes.kt
@@ -0,0 +1,19 @@
+package no.nav.internal
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+
+fun Route.internalRoutes() {
+    route("/internal") {
+        get("/is-alive") {
+            call.respondText("Application is alive!", status = HttpStatusCode.OK)
+        }
+        get("/is-ready") {
+            call.respondText("Application is ready!", status = HttpStatusCode.OK)
+        }
+    }
+}

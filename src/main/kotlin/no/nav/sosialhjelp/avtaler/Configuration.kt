e7d10efbe4d77ea421aeddf6dd928fc820041db1
Line Moseng <line.moseng@nav.no>
2022-11-01 15:19:25 +0100

    kort pdf-navn

--- src/main/kotlin/no/nav/sosialhjelp/avtaler/Configuration.kt
+++ src/main/kotlin/no/nav/sosialhjelp/avtaler/Configuration.kt
@@ -27,7 +27,7 @@ object Configuration {
             "digipost.certificatePassword" to "CertificatePassword",
             "digipost.onCompletionUrl" to "/opprett-avtale/kvittering",
             "digipost.onErrorUrl" to "/opprett-avtale/feil",
-            "digipost.avtalePdfPath" to "Avtaletekst avtale om tilgjengeliggjøring av sentrale stønadsopplysninger.pdf"
+            "digipost.avtalePdfPath" to "Avtale.pdf"
         )
     )
 

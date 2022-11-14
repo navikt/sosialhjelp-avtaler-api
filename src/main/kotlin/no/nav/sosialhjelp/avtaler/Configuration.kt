2e6e0247e72b5450fa6db95c2ca6ad3f259a4c77
Ida Onshus <25682301+idaons@users.noreply.github.com>
2022-11-14 07:21:54 +0100

    NÅ er  password secret version 2

--- src/main/kotlin/no/nav/sosialhjelp/avtaler/Configuration.kt
+++ src/main/kotlin/no/nav/sosialhjelp/avtaler/Configuration.kt
@@ -67,7 +67,7 @@ object Configuration {
             "virksomhetssertifikat.versionId" to "3",
             "virksomhetssertifikat.passwordProjectId" to "virksomhetssertifikat-dev",
             "virksomhetssertifikat.passwordSecretId" to "test-keystore-credentials-json",
-            "virksomhetssertifikat.passwordSecretVersion" to "1",
+            "virksomhetssertifikat.passwordSecretVersion" to "2",
             "pdl.url" to "https://pdl-api.dev-fss-pub.nais.io/graphql",
             "pdl.audience" to "dev-fss:pdl:pdl-api"
         )

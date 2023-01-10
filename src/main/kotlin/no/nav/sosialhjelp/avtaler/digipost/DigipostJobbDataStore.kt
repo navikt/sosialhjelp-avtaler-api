781c07bacf342d15ca3f331ae616d93d53ce5028
Line Moseng <line.moseng@nav.no>
2023-01-10 13:20:18 +0100

    Legger til signert dokument i db

--- src/main/kotlin/no/nav/sosialhjelp/avtaler/digipost/DigipostJobbDataStore.kt
+++ src/main/kotlin/no/nav/sosialhjelp/avtaler/digipost/DigipostJobbDataStore.kt
@@ -23,10 +23,11 @@ class DigipostJobbDataStorePostgres(private val sessionFactory: () -> Session) :
             INSERT INTO digipost_jobb_data (orgnr,
                                             direct_job_reference,
                                             status_url,
-                                            status_query_token)
-            VALUES (:orgnr, :direct_job_reference, :status_url, :status_query_token)
+                                            status_query_token,
+                                            signert_dokument)
+            VALUES (:orgnr, :direct_job_reference, :status_url, :status_query_token, :signert_dokument)
             ON CONFLICT (orgnr) 
-            DO UPDATE SET direct_job_reference = :direct_job_reference, status_url = :status_url, status_query_token = :status_query_token
+            DO UPDATE SET direct_job_reference = :direct_job_reference, status_url = :status_url, status_query_token = :status_query_token, signert_dokument = :signert_dokument
         """.trimIndent()
 
         it.update(
@@ -35,7 +36,8 @@ class DigipostJobbDataStorePostgres(private val sessionFactory: () -> Session) :
                 "orgnr" to digipostJobbData.orgnr,
                 "direct_job_reference" to digipostJobbData.directJobReference,
                 "status_url" to digipostJobbData.statusUrl.toString(),
-                "status_query_token" to digipostJobbData.statusQueryToken
+                "status_query_token" to digipostJobbData.statusQueryToken,
+                "signert_dokument" to digipostJobbData.signertDokument
             )
         )
         digipostJobbData
@@ -72,6 +74,7 @@ class DigipostJobbDataStorePostgres(private val sessionFactory: () -> Session) :
         orgnr = row.string("orgnr"),
         directJobReference = row.string("direct_job_reference"),
         statusUrl = URI(row.string("status_url")),
-        statusQueryToken = row.stringOrNull("status_query_token")
+        statusQueryToken = row.stringOrNull("status_query_token"),
+        signertDokument = row.binaryStreamOrNull("signert_dokument")
     )
 }

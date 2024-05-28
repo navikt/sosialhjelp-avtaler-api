2305fa6259f29521a40039e916d859237444892e
Martin Almvik <martin.almvik@nav.no>
2024-05-28 15:15:46 +0200

    Setter opp for avtaler-admin

--- src/main/kotlin/no/nav/sosialhjelp/avtaler/avtaler/AvtaleService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/avtaler/avtaler/AvtaleService.kt
@@ -121,14 +121,6 @@ class AvtaleService(
         return digipostResponse.redirectUrl
     }
 
-    fun masseSigner(fnr: String): URI {
-        val digipostResponse = digipostService.sendMangeTilSignering(fnr)
-
-//        lagreDigipostResponse(avtaleRequest.orgnr, digipostResponse)
-
-        return digipostResponse.redirectUrl
-    }
-
     private suspend fun lagreDigipostResponse(
         orgnr: String,
         digipostResponse: DigipostResponse,

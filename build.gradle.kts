0445a9726a2490df2a304ceb31f572cc5ce30c2d
Martin Almvik <martin.almvik@nav.no>
2025-06-23 10:30:25 +0200

    Legg til version-catalog-update og kjør den

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.plugin.serialization)
     alias(libs.plugins.ktor)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.version.catalog.update)
 }
 
 ktlint {
